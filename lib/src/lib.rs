5e7a8964cd02ec8fa20548259e12c3939d29a7e4
Ashe Aupepin <ashe.sudosmile@gmail.com>
2022-05-23 17:32:42 +0800

    feat: split functions into modules

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,56 +1,84 @@
-use std::ffi::CStr;
-use std::ffi::CString;
-use std::os::raw::c_char;
-use std::os::raw::c_int;
-
-mod var;
-
 #[no_mangle]
 pub extern "C" fn hello_from_rust() {
     println!("Hello from Rust!");
 }
 
-/// `concat_strs` takes two C strings, converts them to Rust strings, concatenates them, 
-/// and returns a C string
-/// 
-/// Arguments:
-/// 
-/// * `a`: *const c_char
-/// * `b`: *const c_char
-/// 
-/// Returns:
-/// 
-/// A persistent C string
-#[no_mangle]
-pub extern "C" fn concat_strs(a: *const c_char, b: *const c_char) -> *const c_char{
-    let out;
-    unsafe {
-        let a_safe = CStr::from_ptr(a).to_str().unwrap();
-        let b_safe = CStr::from_ptr(b).to_str().unwrap();
-        out = CString::new(format!("{}{}", a_safe, b_safe))
-            .unwrap();
+mod str {
+    use std::ffi::CString;
+    use std::ffi::CStr;
+    use std::os::raw::c_int;
+    use std::os::raw::c_char;
+
+    pub(crate) fn from_cstr(a: *const c_char) -> &'static str {
+        unsafe {  
+            assert!(!a.is_null());
+            CStr::from_ptr(a)
+        }.to_str().unwrap()
     }
-    var::strpersist(out.as_ptr())
+
+    #[no_mangle]
+    pub extern "C" fn len_str(s: *const c_char) -> c_int {
+        let s_safe = from_cstr(s);
+        s_safe.chars().count() as c_int
+    }
+
+    #[no_mangle]
+    pub extern "C" fn cmp_str(a: *const c_char, b: *const c_char) -> c_int {
+        let a_safe = from_cstr(a);
+        let b_safe = from_cstr(b);
+
+        (a_safe == b_safe) as c_int
+    }
+
+    #[no_mangle]
+    pub extern "C" fn dup_str(s: *const c_char) -> *mut c_char {
+        let s_safe = from_cstr(s);
+        let out = CString::new(s_safe).unwrap();
+        out.into_raw()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn concat_str(a: *const c_char, b: *const c_char) -> *mut c_char{
+        let a_safe = from_cstr(a);
+        let b_safe = from_cstr(b); 
+
+        let out = {
+            let mut out_array: Vec<u8> = Vec::new();
+
+            for c in a_safe.chars() {
+               out_array.push(c as u8)
+            }
+            for c in b_safe.chars() {
+                out_array.push(c as u8)
+            }
+
+            CString::new(out_array)
+                .unwrap()
+        };
+        out.into_raw()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn free_str(s: *mut c_char) {
+        unsafe {
+            assert!(!s.is_null());
+            CString::from_raw(s)
+        };
+    }
 }
 
-/// `int_from_str` takes a pointer to a C string, converts it to a Rust string, 
-/// parses it to an integer, and returns the integer
-/// 
-/// Arguments:
-/// 
-/// * `a`: *const c_char
-/// 
-/// Returns:
-/// 
-/// A c_int
-#[no_mangle]
-pub extern "C" fn int_from_str(a: *const c_char) -> c_int{
-    let a_int: c_int;
-    unsafe {
-        let a_safe = CStr::from_ptr(a).to_str().unwrap();
-        a_int = a_safe.parse().unwrap_or_default();
+
+mod int {
+    use std::os::raw::c_int;
+    use std::os::raw::c_char;
+
+    #[no_mangle]
+    pub extern "C" fn int_from_str(s: *const c_char) -> c_int{
+        let s_safe = crate::str::from_cstr(s);
+        //todo: implement parsing an int from a string manually
+        let s_int: c_int = s_safe.parse().unwrap_or_default();
+        s_int
     }
-    a_int
 }
 
 #[cfg(test)]
@@ -60,12 +88,19 @@ mod tests {
     #[test]
     fn test_int_from_str() {
         let c_str1 = std::ffi::CString::new("123".as_bytes()).unwrap();
-        assert_eq!(int_from_str(c_str1.as_ptr()), 123);
+        assert_eq!(int::int_from_str(c_str1.as_ptr()), 123);
 
         let c_str2 = std::ffi::CString::new("abc".as_bytes()).unwrap();
-        assert_eq!(int_from_str(c_str2.as_ptr()), 0);
+        assert_eq!(int::int_from_str(c_str2.as_ptr()), 0);
 
         let c_str3 = std::ffi::CString::new("99999999999999999999999999999".as_bytes()).unwrap();
-        assert_eq!(int_from_str(c_str3.as_ptr()), 0);
+        assert_eq!(int::int_from_str(c_str3.as_ptr()), 0);
     }
+
+    // fn test_concat_string() {
+    //     let c_str1 = std::ffi::CString::new("abc".as_bytes()).unwrap();
+    //     let c_str2 = std::ffi::CString::new("123".as_bytes()).unwrap();
+    //     let out = std::ffi::CString::new("abc123".as_bytes()).unwrap();
+    //     assert_eq!(concat_strs(c_str1.as_ptr(), c_str2.as_ptr())), out);
+    // }
 }
--- src/call_rust.c
+++ src/call_rust.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include "crs.h"
 
+
+
 int main(void) {
-    const char *str = concat_strs("hello ", "world");
+    char *str = concat_str("hello ", "world");
     printf("concat_strs: %s\n", str);
 
-    int i = int_from_str("123");
+    int i = int_from_str("1234");
     printf("int_from_str: %d\n", i);
 
+    printf("cmp result: (%s == hello world) %d\n", str, cmp_str(str, "hello world"));
+
+    free_str(str);
     return 0;
 }
