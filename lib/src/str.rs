4afe1f48a5b059a5dff34ab37ba8de876ba8a25e
Ashe Aupepin <ashe.sudosmile@gmail.com>
2022-05-24 18:01:30 +0800

    fix: removed merge errors

--- lib/src/str.rs
+++ lib/src/str.rs
@@ -18,17 +18,10 @@ fn len_str(s: char_p::Ref<'_>) -> usize {
     s.to_str().len()
 }
 
-<<<<<<< HEAD
-#[no_mangle]
-pub extern "C" fn sep_str(s: *const c_char, separators: *const c_char) -> *mut *mut c_char {
-    let s_safe = from_cstr(s);
-    let separators_safe = from_cstr(separators);
-=======
 #[ffi_export]
 fn sep_str(string: char_p::Ref<'_>, separators: char_p::Ref<'_>) -> safer_ffi::slice::Box<char_p::Box> {
     let s_safe = string.to_str();
     let separators_safe = separators.to_str();
->>>>>>> safer_ffi
 
     let mut str_vec: Vec<char_p::Box> = Vec::new();
     let separator_re = Regex::new(format!("([{}]+)", separators_safe).as_str()).unwrap();
@@ -36,27 +29,6 @@ fn sep_str(string: char_p::Ref<'_>, separators: char_p::Ref<'_>) -> safer_ffi::s
     let split_s: Vec<&str> = separator_re.split(s_safe).into_iter().collect();
 
     for &i in split_s.iter() {
-<<<<<<< HEAD
-        str_vec.push(safe_unwrap(CString::new(i)));
-    }
-    let mut out = str_vec
-        .into_iter()
-        .map(|s| s.into_raw())
-        .collect::<Vec<_>>();
-    out.push(std::ptr::null_mut());
-    out.shrink_to_fit();
-    assert_eq!(out.len(), out.capacity());
-
-    let ptr = out.as_mut_ptr();
-    std::mem::forget(out);
-    ptr
-}
-
-#[no_mangle]
-pub extern "C" fn eq_str(a: *const c_char, b: *const c_char) -> c_int {
-    let a_safe = from_cstr(a);
-    let b_safe = from_cstr(b);
-=======
         str_vec.push(i.to_owned().try_into().unwrap());
     };
     let out: Box<[char_p::Box]> = str_vec.try_into().unwrap();
@@ -67,7 +39,6 @@ pub extern "C" fn eq_str(a: *const c_char, b: *const c_char) -> c_int {
 fn cmp_str(a: char_p::Ref<'_>, b: char_p::Ref<'_>) -> i32 {
     let a_safe = a.to_str();
     let b_safe = b.to_str();
->>>>>>> safer_ffi
 
     (a_safe == b_safe) as i32
 }
--- src/call_rust.c
+++ src/call_rust.c
@@ -7,12 +7,8 @@ int main(void) {
     char *str = concat_str("hello ", "world");
     printf("concat_strs: '%s'\n", str);
 
-<<<<<<< HEAD
-    char intstr[] = "-1234,+123-456+123123123";
-=======
     char intstr[] = "-1234,+123-456+123123123-12,13";
     printf("strdup: %s\n", dup_str(intstr));
->>>>>>> safer_ffi
 
     int i = int_from_str(intstr);
     printf("int_from_str: %d\n", i);
@@ -22,14 +18,8 @@ int main(void) {
         printf("%d\n", ints.ptr[i]);
     }
 
-<<<<<<< HEAD
-    printf("cmp result: (%s == hello world) %d\n", str, eq_str(str, "hello world"));
-
-    char **sepstr = sep_str("hello,world", ",");
-=======
     char sepstr[] = "hello,world !";
     slice_boxed_char_ptr_t slice = sep_str(sepstr, ", ");
->>>>>>> safer_ffi
 
     for (size_t i = 0; i < slice.len; i++) {
         printf("%s\n", slice.ptr[i]);
